7f1bd4d0f3883752e65d5ed1c893bf8a37eb25f1
DayoonHwang <3792390@gmail.com>
2025-12-05 09:41:24 +0900

    Update scripts for resnet18 configuration

--- run.sh
+++ run.sh
@@ -15,17 +15,17 @@ python3 main.py \
 --results_path results \
 --run_name run \
 net \
--b wideresnet50 \
+-b resnet18 \
 -le layer2 \
 -le layer3 \
---pretrain_embed_dimension 1536 \
---target_embed_dimension 1536 \
+--pretrain_embed_dimension 384 \
+--target_embed_dimension 384 \
 --patchsize 3 \
 --meta_epochs 40 \
 --embedding_size 256 \
 --gan_epochs 4 \
 --noise_std 0.015 \
---dsc_hidden 1024 \
+--dsc_hidden 512 \
 --dsc_layers 2 \
 --dsc_margin .5 \
 --pre_proj 1 \
